2f4cd788c6e58b1d6e5e995060aaf5a2cb0bf3ba
sundusk <zhuyue346@gmail.com>
2021-12-20 15:50:10 +0800

    更新第三方库

--- xyTV/xyTV/ViewController.swift
+++ xyTV/xyTV/ViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 import SnapKit
 import Alamofire
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.white
+        
+        
     }
 
 
